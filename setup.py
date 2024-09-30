6a2aee89f7c10b8bea5cbd054c062076d7decad7
Beth Delaney <64157328+bethdelaney@users.noreply.github.com>
2024-09-30 17:58:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     install_requires=[
         'numpy',
         'scipy',
-        'scikit-learn'
-        'raseterio'
+        'scikit-learn',
+        'raseterio',
         'matplotlib'
     ],
     author='Beth Delaney',
